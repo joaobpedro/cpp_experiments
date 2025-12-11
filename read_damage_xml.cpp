91eed35a8bf06998c12a44d304151df7721c1de0
João Pedro <joao.pedro@km.kongsberg.com>
2025-12-11 16:30:08 +0100

    extracting damage per damage location on the way

--- read_damage_xml.cpp
+++ read_damage_xml.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector> //this is a list basically
 
 
 void clean_string(std::string &cleanee, const std::string &cleaner) {
@@ -19,6 +20,55 @@ void clean_string(std::string &cleanee, const std::string &cleaner) {
   }
 
   std::string clean_string = cleanee.erase(start_pos, end_position-1);
+}
+
+
+std::vector<std::string> split(const std::string& s, char seperator) {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream token_stream(s);
+
+  while (std::getline(token_stream, token, seperator)) {
+    tokens.push_back(token);
+  }
+
+  return tokens;
+}
+
+
+std::string get_max_damage(const std::string& xml_data, const std::string location){
+  // the function gets a location and returns the maximum damage for that position
+
+  std::string start_tag = "<fx_gpos " + location + ">";
+  std::string end_tag = "</fx_pos>";  
+  std::string damage_tag = "<fx_damage_wire_pos";
+  std::string damage_str;
+  size_t pointer_position;
+
+  size_t start_position = xml_data.find(start_tag);
+  if (start_position == std::string::npos) {
+    return "";
+  }
+
+  pointer_position = start_position;
+
+  while (true) {
+    size_t damage_pos = xml_data.find(damage_tag, pointer_position);
+    if (damage_pos == std::string::npos) {
+      break;
+    }
+    damage_pos += damage_tag.length(); // advancing the pointer to the end of the tag
+    // the size of the damage word is hard coded
+    size_t chunk = 32 * sizeof(char);
+    damage_str = xml_data.substr(damage_pos, chunk);
+    clean_string(damage_str, " damage=\"");
+    clean_string(damage_str,"\"");
+
+    pointer_position += damage_pos;
+    // std::cout << damage_str << std::endl;
+  }
+
+  return damage_str;
 }
 
 
@@ -135,9 +185,17 @@ int main() {
 
   // print all positions as test
   std::cout << "------- printing the gpos data --------" << std::endl;
-  std::cout << get_all_gpos(file_content, "fx_gpos") << std::endl;
+  std::string gpos_2 = get_all_gpos(file_content, "fx_gpos");
+  std::cout << gpos_2 << std::endl;
 
-  std::cout << "This is a test" << std::endl;
+  std::vector<std::string> gpos_list = split(gpos_2, ' ');
+  for (const auto& gpos : gpos_list) {
+    std::cout << gpos << std::endl;
+    // get_max_damage(file_content, gpos);
+    // break;
+  }
+  std::string string_test = get_max_damage(file_content, gpos_list[1]);
+  std::cout << string_test << std::endl;
 
   return 0;
 }
